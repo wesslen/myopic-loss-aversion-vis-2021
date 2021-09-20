2e2019750813fdf2cd6a79c3d80ef445e081211c
Ryan Wesslen <rhymenoceros@MacBook-Air-3.local>
2021-09-20 11:17:51 -0400

    remove .R installs for binder

--- install.R
+++ install.R
@@ -1,13 +1,13 @@
 # Package names
-packages <- c("ggdist","patchwork","scales","rmarkdown","knitr","mongolite","anytime","assertthat","feather","ggrepel","ggridges","ggpubr","boot","Hmisc","brms","modelr","parallel","rstan","tidybayes","bayesplot","RColorBrewer","grid","pBrackets")
-
-# Install packages not yet installed
-installed_packages <- packages %in% rownames(installed.packages())
-if (any(installed_packages == FALSE)) {
-  install.packages(packages[!installed_packages])
-}
-
-# colorblindr
-remotes::install_github("wilkelab/cowplot")
-install.packages("colorspace", repos = "http://R-Forge.R-project.org")
-remotes::install_github("clauswilke/colorblindr")
+# packages <- c("ggdist","patchwork","scales","rmarkdown","knitr","mongolite","anytime","assertthat","feather","ggrepel","ggridges","ggpubr","boot","Hmisc","brms","modelr","parallel","rstan","tidybayes","bayesplot","RColorBrewer","grid","pBrackets")
+# 
+# # Install packages not yet installed
+# installed_packages <- packages %in% rownames(installed.packages())
+# if (any(installed_packages == FALSE)) {
+#   install.packages(packages[!installed_packages])
+# }
+# 
+# # colorblindr
+# remotes::install_github("wilkelab/cowplot")
+# install.packages("colorspace", repos = "http://R-Forge.R-project.org")
+# remotes::install_github("clauswilke/colorblindr")
